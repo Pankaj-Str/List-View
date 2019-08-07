ffc445a4f854f86a60605c3976c966b2d0e17e9e
p4ntechnology <pankajchouhan80@gmail.com>
2019-08-07 17:25:41 +0530

    first commit

---
+++ app/src/main/java/p4n/in/listview/MainActivity.java
@@ -0,0 +1,39 @@
+package p4n.in.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    String[] listItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView)findViewById(R.id.listView);
+
+        listItem = getResources().getStringArray(R.array.array_technology);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // TODO Auto-generated method stub
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),"welcome to :- "+value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
